21edd8a5c4492e790c5093f3e50d3bcaaac1ac8a
Aiko <1603888371@qq.com>
2021-01-31 22:55:45 +0800

    提交java

---
+++ basic-code/day01-test/src/com/xhyj/com/Test.java
@@ -0,0 +1,12 @@
+package com.xhyj.com;
+
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Aiko,I love you !!!!");
+
+        Scanner sc = null;
+    }
+}
